ad9f67f719b39f75cde8b6d227caa5b0b2ea3974
yudaming <mingjay100@163.com>
2017-05-12 14:06:13 +0800

    商品主页九宫格的添加

--- src/pages/GoodsList.vue
+++ src/pages/GoodsList.vue
@@ -1,20 +1,33 @@
 <template>
   <div class="demo">
+    <group-title></group-title>
+    <grid :rows="4">
+      <grid-item v-for="i in 8"
+                 :key="i">
+        <img slot="icon"
+             src="../mock/images/pic1.png">
+      </grid-item>
+    </grid>
   </div>
 </template>
 
 <script>
-import { Group, Cell } from 'vux'
+import { Grid, GridItem, GroupTitle } from 'vux'
 
 export default {
-  name: '',
   components: {
-    Group,
-    Cell
+    Grid,
+    GridItem,
+    GroupTitle
+  },
+  methods: {
+    onItemClick () {
+      alert('123')
+    }
   }
 }
 </script>
 
-<style scoped>
+<style scoped lange="less">
 
 </style>
