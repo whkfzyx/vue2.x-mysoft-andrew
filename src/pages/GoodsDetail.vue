b58c9ca37035c60ce54fd91b2e73a4d20eaed081
zhouy10 <zhouy10@WH-PC091>
2017-05-16 23:33:16 +0800

    完成单页面基本功能

--- src/pages/GoodsDetail.vue
+++ src/pages/GoodsDetail.vue
@@ -1,60 +1,87 @@
 <template>
   <div class="demo">
-    <div>
-        <img src="/image/pic14.png"  class="big-img"/>
-    </div>
-     <group>
-      <form-preview header-label="产品名称"
-                    :header-value="headervalue"
-                    :body-items="list"></form-preview>
-    </group>
+  
+    <img :src="imgurl"
+         class="big-img" />
+    <form-preview header-label="产品名称"
+                  :header-value="headervalue"
+                  :body-items="list"
+                  :footer-buttons="btnconfirm"></form-preview>
+    <date-format></date-format>
   </div>
 </template>
 
 <script>
-import { Group, Cell, FormPreview } from 'vux'
+import {
+  Group,
+  Cell,
+  FormPreview,
+  dateFormat,
+  Clocker
+} from 'vux'
 export default {
   name: 'demo',
   components: {
     Group,
     Cell,
-    FormPreview
+    FormPreview,
+    dateFormat,
+    Clocker
   },
   data () {
     return {
-      list: [
-      //   {label: '当前状态', value: '待还中'},
-      //  {label: '领取时间', value: '2015-09-14 01:02:21'},
-      //  {label: '应还时间', value: '2015-09-14 01:02:21'},
-      //  {label: '领取流水', value: '121718910134150928210680762'},
-      //  {label: '产品编号', value: '00132450001'},
-      //  {label: '隶属部门', value: '行政'}
-      ],
+      list: [],
       imgurl: '',
-      headervalue: ''
+      headervalue: '',
+      btnconfirm: [{
+        style: 'primary',
+        text: '确定',
+        onButtonClick: (name) => {
+          window.history.go(-1)
+        }
+      }]
     }
   },
   methods: {
     loadList () {
-      var grouplist = []
       this.$http.get('/getmyborrowdetail').then((res) => {
         var d = res.data.data
         this.imgurl = d.img
         this.headervalue = d.name
-        grouplist.push({label: '当前状态', value: '待还中'})
-        grouplist.push({label: '领取时间', value: '2015-09-14 01:02:21'})
-        grouplist.push({label: '应还时间', value: d.shouldReturnDate})
-        grouplist.push({label: '领取流水', value: d.orderId})
-        grouplist.push({label: '产品编号', value: d.assetSN})
-        grouplist.push({label: '隶属部门', value: d.department})
-      }).catch((res) => { console.log(res) })
+
+        this.list.push({
+          label: '当前状态',
+          value: '待还中'
+        })
+        this.list.push({
+          label: '领取时间',
+          value: dateFormat(d.date, 'YYYY-MM-DD HH:mm:ss')
+        })
+        this.list.push({
+          label: '应还时间',
+          value: dateFormat(d.shouldReturnDate, 'YYYY-MM-DD HH:mm:ss')
+        })
+        this.list.push({
+          label: '领取流水',
+          value: d.orderId
+        })
+        this.list.push({
+          label: '产品编号',
+          value: d.assetSN
+        })
+        this.list.push({
+          label: '隶属部门',
+          value: d.department
+        })
+      }).catch((res) => {
+        console.log(res)
+      })
     }
   },
   created () {
     this.loadList()
   }
 }
-
 </script>
 
 <style lang="less">
